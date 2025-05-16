e419f68aa07e2da5e8e9479e0660fb8f522d359b
kolegasior <jkole822@gmail.com>
2025-05-16 00:36:28 -0400

    test pre-commit fmt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,8 +13,7 @@ fn main() {
     
         let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
+        io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
